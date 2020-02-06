a27dc42ae20e5a17261a5f5bcaaa248c0a510832
shun-cheng <shuncheng.wu91@gmail.com>
2020-02-06 06:13:57 +0100

    add generation for final level

--- src/train_data_gen.py
+++ src/train_data_gen.py
@@ -25,13 +25,15 @@ flags.DEFINE_string('base_dir', '',
                     'Root directory. Expects a directory containing the model.')
 flags.DEFINE_string('input_dir', '/media/sc/BackupDesk/TrainingData_TSDF/train_SceneNetRGBD_3_level',
                     'Directory to input TFRecords.')
-flags.DEFINE_string('output_dir', '/media/sc/BackupDesk/TrainingData_TSDF/train_SceneNetRGBD_3_level_pred', '')
-flags.DEFINE_string('model_path', '/home/sc/research/ScanComplete/train/train_v003', '')
+flags.DEFINE_string('predict_dir', '/media/sc/BackupDesk/TrainingData_TSDF/train_SceneNetRGBD_3_level_pred',
+                    'Directory to input TFRecords.')
+flags.DEFINE_string('output_dir', '/media/sc/BackupDesk/TrainingData_TSDF/train_SceneNetRGBD_3_level_pred_2', '')
+flags.DEFINE_string('model_path', '/home/sc/research/ScanComplete/train/train_v002', '')
 flags.DEFINE_string('model_checkpoint', '',
                     'Model checkpoint to use (empty for latest).')
-flags.DEFINE_integer('hierarchy_level', 3, 'Hierachy level (1: finest level).')
+flags.DEFINE_integer('hierarchy_level', 2, 'Hierachy level (1: finest level).')
 flags.DEFINE_integer('num_quant_levels', 256, 'Number of quantization bins.')
-flags.DEFINE_bool('is_base_level', True, 'If base level of hierarchy.')
+flags.DEFINE_bool('is_base_level', False, 'If base level of hierarchy.')
 flags.DEFINE_integer('pad_test', 0, 'Scene padding.')
 flags.DEFINE_integer('p_norm', 1, 'P-norm loss (0 to disable).')
 flags.DEFINE_bool('predict_semantics', True,
@@ -91,8 +93,29 @@ def read_input_bytes_feature(feature_map, key, shape):
             dim_z, dim_y, dim_x)
     return tensor
 
+def read_prediction(hierarchy_level, feature_map, num_quant_levels, p_norm,
+                predict_semantics, shape = [16,16,16]):
+    
+    key_samples = 'samples_' + _RESOLUTIONS[hierarchy_level] 
+    key_samples_sem = 'sem_samples_' + _RESOLUTIONS[hierarchy_level] 
+     
+    prediction_scan_low_resolution = read_input_float_feature(
+          feature_map, key_samples, shape)
+    (scene_dim_z, scene_dim_y,
+        scene_dim_x) = prediction_scan_low_resolution.shape
+    prediction_scan_low_resolution = util.preprocess_target_sdf(
+          prediction_scan_low_resolution, num_quant_levels, constants.TRUNCATION,
+          p_norm == 0)
+    
+    
+    if predict_semantics:
+        prediction_semantics_low_resolution = read_input_bytes_feature(
+            feature_map, key_samples_sem,
+            [scene_dim_z, scene_dim_y, scene_dim_x])
+    return prediction_scan_low_resolution, prediction_semantics_low_resolution
+  
 
-def read_inputs(hierarchy_level, filename, feature_map, height, padding, num_quant_levels, p_norm,
+def read_inputs(hierarchy_level, feature_map, height, padding, num_quant_levels, p_norm,
                 predict_semantics, processing, shape = [16,16,16]):
     """Reads inputs for scan completion.
     
@@ -157,38 +180,6 @@ def read_inputs(hierarchy_level, filename, feature_map, height, padding, num_qua
         if target_semantics is None:
           target_semantics = np.zeros([scene_dim_z, scene_dim_y, scene_dim_x])
       
-        # Load previous level prediction.
-        # if not FLAGS.is_base_level:
-        #   previous_file = os.path.join(
-        #       FLAGS.output_dir_prev, 'level' + str(FLAGS.hierarchy_level - 1) + '_' +
-        #       os.path.splitext(os.path.basename(filename))[0] + 'pred.tfrecord')
-        #   tf.logging.info('Reading previous predictions frome file: %s',
-        #                   previous_file)
-        #   assert os.path.isfile(previous_file)
-        #   for record in tf.python_io.tf_record_iterator(previous_file):
-        #     prev_example = tf.train.Example()
-        #     prev_example.ParseFromString(record)
-        #     prev_feature_map = prev_example.features
-        #   prediction_scan_low_resolution = read_input_float_feature(
-        #       prev_feature_map, 'prediction_df', None)
-        #   (prev_scene_dim_z, prev_scene_dim_y,
-        #    prev_scene_dim_x) = prediction_scan_low_resolution.shape
-        #   offset_z = (prev_scene_dim_z - scene_dim_z // 2) // 2
-        #   offset_x = (prev_scene_dim_x - scene_dim_x // 2) // 2
-        #   prediction_scan_low_resolution = prediction_scan_low_resolution[
-        #       offset_z:offset_z + scene_dim_z // 2, :scene_dim_y // 2, offset_x:
-        #       offset_x + scene_dim_x // 2]
-        #   prediction_scan_low_resolution = util.preprocess_target_sdf(
-        #       prediction_scan_low_resolution, num_quant_levels, constants.TRUNCATION,
-        #       p_norm == 0)
-        #   if predict_semantics:
-        #     prediction_semantics_low_resolution = read_input_bytes_feature(
-        #         prev_feature_map, 'prediction_sem',
-        #         [prev_scene_dim_z, prev_scene_dim_y, prev_scene_dim_x])
-        #     prediction_semantics_low_resolution = prediction_semantics_low_resolution[
-        #         offset_z:offset_z + scene_dim_z // 2, :scene_dim_y // 2, offset_x:
-        #         offset_x + scene_dim_x // 2]
-                
     return (input_scan, target_scan, target_semantics,
             prediction_scan_low_resolution, prediction_semantics_low_resolution)
 
@@ -395,68 +386,105 @@ def Feature(sdf, gt, gt_df, hierarchy_level=1):
     feature = tf.train.Example(features=tf.train.Features(feature=get_dict(sdf,gt,gt_df,hierarchy_level-1)))
     return feature.SerializeToString()
     
-def process(path, path_out):
+def process(path, path_out, path_pred=None):
     counter=0
     
     pred = Prediction([ _DIMS[FLAGS.hierarchy_level-1],  _DIMS[FLAGS.hierarchy_level-1],  _DIMS[FLAGS.hierarchy_level-1]], 
                   FLAGS.model_path)
     
     with tf.io.TFRecordWriter(path_out) as writer:
-        for record in tf.python_io.tf_record_iterator(path):
-            counter += 1
-            
-            example = tf.train.Example()
-            example.ParseFromString(record)
-            feature_map = example.features
-            
-            
-            
-            # get raw data from input file
-            serialization=dict()
-            serialization['data'] = _bytes_feature(tf.train.Example(features=feature_map).SerializeToString())
-
-            # for level in range(len(_RESOLUTIONS)):
-            #     (input_scan, target_scan, target_semantics, prediction_scan_low_resolution,
-            #       prediction_semantics_low_resolution) = read_inputs(
-            #       level, FLAGS.input_dir, feature_map, _DIMS[level], 0,
-            #       FLAGS.num_quant_levels, FLAGS.p_norm, FLAGS.predict_semantics, processing=0,
-            #       shape=[ _DIMS[level], _DIMS[level], _DIMS[level]])
-                      
-            #     serialized = get_dict(input_scan, target_semantics, target_scan, level)
-            #     for key, value in serialized.items():
-            #         serialization[key] = value
-                  
-            
-            if FLAGS.hierarchy_level > len(_RESOLUTIONS):
-                raise RuntimeError("out of range")
-            key_samples = 'samples_' + _RESOLUTIONS[FLAGS.hierarchy_level-1] 
-            key_samples_sem = 'sem_samples_' + _RESOLUTIONS[FLAGS.hierarchy_level-1] 
-            
-            # predict 
-            ## load data at previous level
-            (input_scan, target_scan, target_semantics, prediction_scan_low_resolution,
-              prediction_semantics_low_resolution) = read_inputs(
-              FLAGS.hierarchy_level-1, FLAGS.input_dir, feature_map, _DIMS[FLAGS.hierarchy_level-1], 0,
-              FLAGS.num_quant_levels, FLAGS.p_norm, FLAGS.predict_semantics, processing=1,
-              shape=[ _DIMS[FLAGS.hierarchy_level-1], _DIMS[FLAGS.hierarchy_level-1], _DIMS[FLAGS.hierarchy_level-1]])
-            ## predict
-            output_prediction_scan,output_prediction_semantics = \
-                pred.predict(input_scan, target_scan, target_semantics, prediction_scan_low_resolution, 
-                  prediction_semantics_low_resolution)
+        if FLAGS.is_base_level:
+            for record in tf.python_io.tf_record_iterator(path):
+                counter += 1
                 
-            serialization[key_samples] = _float_feature(output_prediction_scan.ravel())
-            serialization[key_samples_sem] =_bytes_feature(output_prediction_semantics.tobytes())
+                example = tf.train.Example()
+                example.ParseFromString(record)
+                feature_map = example.features
+                
+                # get raw data from input file
+                serialization=dict()
+                serialization['data'] = _bytes_feature(tf.train.Example(features=feature_map).SerializeToString())
+    
+                if FLAGS.hierarchy_level > len(_RESOLUTIONS):
+                    raise RuntimeError("out of range")
+                key_samples = 'samples_' + _RESOLUTIONS[FLAGS.hierarchy_level-1] 
+                key_samples_sem = 'sem_samples_' + _RESOLUTIONS[FLAGS.hierarchy_level-1] 
+                
+                # predict 
+                ## load data at previous level
+                (input_scan, target_scan, target_semantics, prediction_scan_low_resolution,
+                  prediction_semantics_low_resolution) = read_inputs(
+                  FLAGS.hierarchy_level-1, FLAGS.input_dir, feature_map, _DIMS[FLAGS.hierarchy_level-1], 0,
+                  FLAGS.num_quant_levels, FLAGS.p_norm, FLAGS.predict_semantics, processing=1,
+                  shape=[ _DIMS[FLAGS.hierarchy_level-1], _DIMS[FLAGS.hierarchy_level-1], _DIMS[FLAGS.hierarchy_level-1]])
+                ## predict
+                output_prediction_scan,output_prediction_semantics = \
+                    pred.predict(input_scan, target_scan, target_semantics, prediction_scan_low_resolution, 
+                      prediction_semantics_low_resolution)
                     
-            feature = tf.train.Example(features=tf.train.Features(feature=serialization))
-            
-            
-            writer.write(feature.SerializeToString())
-           
-            if FLAGS.debug is True:
-                print('counter: ', counter)
-                if counter > 10:
-                    break
-        print('counter:',counter)
+                serialization[key_samples] = _float_feature(output_prediction_scan.ravel())
+                serialization[key_samples_sem] =_bytes_feature(output_prediction_semantics.tobytes())
+                        
+                feature = tf.train.Example(features=tf.train.Features(feature=serialization))
+                
+                
+                writer.write(feature.SerializeToString())
+               
+                if FLAGS.debug is True:
+                    print('counter: ', counter)
+                    if counter > 10:
+                        break
+            print('counter:',counter)
+        else:
+            for record, record_pred in zip(tf.python_io.tf_record_iterator(path),\
+                                           tf.python_io.tf_record_iterator(path_pred)):
+                counter += 1
+                
+                example = tf.train.Example()
+                example.ParseFromString(record)
+                feature_map = example.features
+                example.ParseFromString(record_pred)
+                feature_map_pred = example.features
+                
+                # get raw data from input file
+                serialization=dict()
+                serialization['data'] = _bytes_feature(tf.train.Example(features=feature_map).SerializeToString())
+    
+                if FLAGS.hierarchy_level > len(_RESOLUTIONS):
+                    raise RuntimeError("out of range")
+                key_samples = 'samples_' + _RESOLUTIONS[FLAGS.hierarchy_level-1] 
+                key_samples_sem = 'sem_samples_' + _RESOLUTIONS[FLAGS.hierarchy_level-1] 
+                
+                # predict 
+                ## load data at previous level
+                (input_scan, target_scan, target_semantics, prediction_scan_low_resolution,
+                  prediction_semantics_low_resolution) = read_inputs(
+                  FLAGS.hierarchy_level-1, feature_map, _DIMS[FLAGS.hierarchy_level-1], 0,
+                  FLAGS.num_quant_levels, FLAGS.p_norm, FLAGS.predict_semantics, processing=1,
+                  shape=[ _DIMS[FLAGS.hierarchy_level-1], _DIMS[FLAGS.hierarchy_level-1], _DIMS[FLAGS.hierarchy_level-1]])
+                
+                prediction_scan_low_resolution,prediction_semantics_low_resolution = read_prediction(
+                    FLAGS.hierarchy_level, feature_map_pred, FLAGS.num_quant_levels, FLAGS.p_norm, FLAGS.predict_semantics,
+                    shape=[ _DIMS[FLAGS.hierarchy_level], _DIMS[FLAGS.hierarchy_level], _DIMS[FLAGS.hierarchy_level]])
+                      
+                      ## predict
+                output_prediction_scan,output_prediction_semantics = \
+                    pred.predict(input_scan, target_scan, target_semantics, prediction_scan_low_resolution, 
+                      prediction_semantics_low_resolution)
+                    
+                serialization[key_samples] = _float_feature(output_prediction_scan.ravel())
+                serialization[key_samples_sem] =_bytes_feature(output_prediction_semantics.tobytes())
+                        
+                feature = tf.train.Example(features=tf.train.Features(feature=serialization))
+                
+                
+                writer.write(feature.SerializeToString())
+               
+                if FLAGS.debug is True:
+                    print('counter: ', counter)
+                    if counter > 10:
+                        break
+            print('counter:',counter)
     
    
 if __name__ == '__main__':
@@ -473,19 +501,21 @@ if __name__ == '__main__':
         
         in_path = os.path.join(FLAGS.input_dir,input_folder_names[i])
         out_path = os.path.join(FLAGS.output_dir, output_file_name)
+        in_path_pred = os.path.join(FLAGS.predict_dir, input_folder_names[i])
         
         print('input:', in_path)
+        print('in_path_pred:', in_path_pred)
         print('out_path:', out_path)
         
     
     
         if FLAGS.debug:
-            process(in_path,out_path)
+            process(in_path,out_path,in_path_pred)
             break
         else:
             results.append(
                 pool.apply_async(process, 
-                                  (in_path,out_path))
+                                  (in_path,out_path,in_path_pred))
                 )
     
     pool.close()
